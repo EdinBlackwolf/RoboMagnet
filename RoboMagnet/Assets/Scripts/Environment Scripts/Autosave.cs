b2852933ea4432a0148edbef1f712f0a3c5ad936
Edin <edinkarakurt99@gmail.com>
2019-04-22 22:51:55 +0300

    Basladik

---
+++ RoboMagnet/Assets/Scripts/Environment Scripts/Autosave.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+//Everytime you press play this script will autosave your scene.
+
+[InitializeOnLoad]
+public class AutoSave
+{
+    // Static constructor that gets called when unity fires up.
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += (PlayModeStateChange state) => {
+            // If we're about to run the scene...
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                // Save the scene and the assets.
+                Debug.Log("Auto-saving all open scenes... " + state);
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
